a5d1f42191548dc801032bdf06ea9ef1c2fd3184
fialho <danidaninpce3@gmail.com>
2021-04-09 16:22:40 -0300

    Setting walk and Idle animation of Player

---
+++ Scripts/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    private Animator _animator;
+   
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetWalkAnimation(bool isWalk)
+    {
+        _animator.SetBool("isWalk",isWalk);
+    }
+    void Update()
+    {
+        
+    }
+}
